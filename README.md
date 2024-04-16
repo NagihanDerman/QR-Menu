7e4a14e04e225a3545b23d3d990f60e37b0b7877
NagihanDerman <dermannagihan@gmail.com>
2024-04-16 19:20:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## QR-Menu
+- Ce projet contient une application JavaScript utilisée pour filtrer les éléments de menu sur une page web et accéder aux informations détaillées sur chaque élément de menu lorsqu'on clique dessus. 
+- Les utilisateurs peuvent consulter les éléments de menu selon des catégories spécifiques et accéder aux détails des produits en cliquant sur les éléments de menu.
+
+## Technologies Utilisées
+- JavaScript
+- HTML
+- CSS
+- Bootstrap
+- Design Responsive
+
+## Gif
---
+++ js/db.js
@@ -0,0 +1,107 @@
+export const menu = [
+  {
+    id: 1,
+    title: "Pancake",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: "The taste of milk and butter in pancakes! Add joy to your day with our soft and fluffy pancakes.",
+  },
+  {
+    id: 2,
+    title: "Diner double hamburger",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: "Our double hamburger with its delicious taste and substantial content  is a perfect choice for lunch meals!",
+  },
+  {
+    id: 3,
+    title: "Godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: "An epic flavor awaits you! Treat yourself with our Godzilla Milkshake and take a sweet break.",
+  },
+  {
+    id: 4,
+    title: "Village Delight: ",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: "The irresistible flavor of village breakfasts is now here! Made with entirely natural ingredients, our village breakfast invites you to a delightful taste experience.",
+  },
+  {
+    id: 5,
+    title: "Egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: "The irresistible flavor of village breakfasts is now here! Made with entirely natural ingredients, our village breakfast invites you to a delightful taste experience.",
+  },
+
+  {
+    id: 6,
+    title: "Oreo Dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: "For Oreo lovers, a magnificent flavor awaits! Oreo Dream takes you to a sweet world. Its taste is truly irresistible!",
+  },
+  {
+    id: 7,
+    title: "Salami Seli",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc:"The breakfast essential! Salami Delight, prepared with plenty of freshly grilled salami and other flavors, will ensure you start your day wonderfully.",
+  },
+  {
+    id: 8,
+    title: "American Classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: "This dish, prepared with the classic flavors of American cuisine, offers an unparalleled taste experience. Moreover, it's also a preferred choice with its reasonable price!",
+  },
+  {
+    id: 9,
+    title:  "Quarantine Buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: "During quarantine days, when you feel bored, indulge in a delicious milkshake that won't leave you alone! Treat yourself to a sweet escape with Quarantine Buddy.",
+  },
+  {
+    id: 10,
+    title: "Bison Steak",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: 
+    "The unique flavor of bison meat combines with special spices and sauces. Bison Steak is a perfect choice for your dinner!",
+  },
+];
+
+export const buttonsData = [
+  {
+    id: 1,
+    text: "ALL",
+    value: "all",
+  },
+  {
+    id: 2,
+    text: "Breakfast",
+    value: "breakfast",
+  },
+  {
+    id: 3,
+    text: "Lunch",
+    value: "lunch",
+  },
+  {
+    id: 4,
+    text: "Shakes",
+    value: "shakes",
+  },
+];
